9f22ec5eccf1159b67807479abe7f08c8321eb87
Edward P. Kelly <ekelly@EdK-MBP.local>
2016-03-28 13:45:40 -0400

    Completing Lesson/Part 11 Section 8/Lecture 107.

--- devslopes-pokedex/PokemonDetailVC.swift
+++ devslopes-pokedex/PokemonDetailVC.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
+    @IBOutlet weak var nameLbl:UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     var pokeItem:Pokemon!
     
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        nameLbl.text = pokeItem.name.capitalizedString
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onBackButtonTapped(sender: UIButton)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
